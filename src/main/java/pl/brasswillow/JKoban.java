dab5f0a8ba20682bc427672c94484c4091f89547
rlojek <rafal.lojek@gmial.com>
2016-09-06 21:38:33 +0200

    Maven Project Commit

---
+++ src/main/java/pl/brasswillow/JKoban.java
@@ -0,0 +1,9 @@
+package pl.brasswillow;
+
+public class JKoban
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pl/brasswillow/JKobanTest.java
@@ -0,0 +1,6 @@
+package pl.brasswillow;
+
+public class JKobanTest {
+
+
+}
